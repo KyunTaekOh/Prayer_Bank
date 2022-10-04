d2cf412a51875d75a2e67232bcc60858944982f2
ynnyn <101929276+ynnyn@users.noreply.github.com>
2022-10-04 23:08:15 +0900

    Initial commit

---
+++ app/src/main/java/edu/handong/prayerbank/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.handong.prayerbank
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
